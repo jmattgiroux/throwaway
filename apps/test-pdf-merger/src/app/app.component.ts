b5abecb798da8fd4b0b194c70d7f40ab38140521
jmattgiroux <jmattgiroux@gmail.com>
2023-02-13 17:34:40 -0600

    Added webpack-bundle-analyzer and used it

--- apps/test-pdf-merger/src/app/app.component.ts
+++ apps/test-pdf-merger/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import PDFMerger from 'pdf-merger-js';
-import {combineLatest, Observable} from 'rxjs';
+import {combineLatest, mergeMap, Observable} from 'rxjs';
 
 const merger = new PDFMerger()
 
@@ -15,28 +15,30 @@ export class AppComponent {
   title = 'test-pdf-merger';
 
   mergePdfs() {
-    const pdf1 = new Observable(observer => {
-      merger.add('https://yes-pdf.com/electronic-book/1906')
+    const pdf1$ = new Observable(observer => {
+      merger.add('https://estimate-3.s3.us-west-2.amazonaws.com/work-order/60ccf35cba8390e35199d652')
         .then((res) => observer.next(res))
     })
 
-    const pdf2 = new Observable(observer => {
-      merger.add('https://yes-pdf.com/electronic-book/1906')
+    const pdf2$ = new Observable(observer => {
+      merger.add('https://estimate-3.s3.us-west-2.amazonaws.com/work-order/60cbfffc2ec5abcd5f522193')
         .then((res) => observer.next(res))
     })
 
-    const pdf3 = new Observable(observer => {
-      merger.add('https://yes-pdf.com/electronic-book/1906')
+    const pdf3$ = new Observable(observer => {
+      merger.add('C:/Repos/throwaway/apps/test-pdf-merger/src/assets/myPDFs/num1.pdf')
         .then((res) => observer.next(res))
     })
 
-    const save = new Observable(observer => {
-      merger.add('https://yes-pdf.com/electronic-book/1906')
+    const save$ = new Observable(observer => {
+      merger.save('myNewFile' )
         .then((res) => observer.next(res))
     })
 
-    combineLatest([pdf1, pdf2, pdf3, save]).subscribe(([pdf1, pdf2, pdf3, save]) => {
-      console.log('pdf1', pdf1)
+    combineLatest([pdf1$, pdf2$])
+      .pipe(mergeMap(([pdf1, pdf2]) => save$))
+      .subscribe((response) => {
+      console.log('response', response)
     }, error => {
       console.log('error', error)
     })
