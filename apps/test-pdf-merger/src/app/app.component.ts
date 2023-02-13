1dce6cb6070f10fd2a0ef58a310e16dcce33a9d8
jmattgiroux <jmattgiroux@gmail.com>
2023-02-13 15:31:11 -0600

    Initial commit

---
+++ apps/test-pdf-merger/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'throwaway-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-pdf-merger';
+}
