c2380f5542574c207265711eb0f8942b8fe00472
jmattgiroux <jmattgiroux@gmail.com>
2023-02-13 16:31:56 -0600

    Added button for merging pdfs

--- apps/test-pdf-merger/src/app/app.component.ts
+++ apps/test-pdf-merger/src/app/app.component.ts
@@ -3,6 +3,8 @@ import PDFMerger from 'pdf-merger-js';
 
 const merger = new PDFMerger()
 
+
+
 @Component({
   selector: 'throwaway-root',
   templateUrl: './app.component.html',
@@ -10,4 +12,26 @@ const merger = new PDFMerger()
 })
 export class AppComponent {
   title = 'test-pdf-merger';
+
+  async mergePdfs() {
+    await merger.add('https://yes-pdf.com/electronic-book/1906')
+    await merger.add('https://yes-pdf.com/electronic-book/4500')
+    await merger.add('https://yes-pdf.com/electronic-book/4497')
+    return merger.save("mynewfile.pdf")
+  }
 }
+
+
+// blobToBase64(res, picture) {
+//   return new Observable(observer => {
+//     const reader = new FileReader();
+//     reader.readAsDataURL(res);
+//     reader.onloadend = function() {
+//       observer.next({
+//         source: reader.result,
+//         name: picture.name,
+//         description: picture.description
+//       });
+//     };
+//   });
+// }
